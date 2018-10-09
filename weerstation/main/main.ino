b9804760743ec7fd3ad349e352ec13f4fd2868b3
rickts98 <ricktersteege@gmail.com>
2018-10-09 15:35:07 +0200

    Update weerstation/main/main.ino

--- weerstation/main/main.ino
+++ weerstation/main/main.ino
@@ -1,20 +1,17 @@
 void setup() {
-<<<<<<< HEAD
+
   // put your setup code here, to run once:
 Serial.begin(9600);
+  ldrSetup();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-sendTempString();
-=======
-  ldrSetup();
-  Serial.begin(9600);
+
+
+
+  Serial.println(readTempValueRAW());
+  sendTempString();
 }
 
-void loop() {
-  Serial.println(getLDRValue());
->>>>>>> ddf972e833cf203b481a5701d7825da0744a2f51
 
-Serial.println(readTempValueRAW());
-}
