61d34bf309ea5630f865d1804f9d2507b162e4d0
rickts98 <ricktersteege@gmail.com>
2018-10-09 14:53:39 +0200

    Update weerstation/main/main.ino

--- weerstation/main/main.ino
+++ weerstation/main/main.ino
@@ -1,17 +1,9 @@
 void setup() {
   // put your setup code here, to run once:
-<<<<<<< HEAD
-//teststststst1
-=======
-<<<<<<< HEAD
-adsfdsaf
-=======
-//teststststst
->>>>>>> 482227ff4e9f36cdc554213de75975455efa221f
->>>>>>> 2c4b591855d093d92d1c9202334ab98d3650b8c7
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-dsafsa
-}dfsafds
+
+}
