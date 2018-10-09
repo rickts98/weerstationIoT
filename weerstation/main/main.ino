283ffb8c206b40cce5bd99b16481968231772c41
Rens Aerts <rens.aerts@ziggo.nl>
2018-10-09 15:40:13 +0200

    Update weerstation/main/main.ino

--- weerstation/main/main.ino
+++ weerstation/main/main.ino
@@ -1,20 +1,10 @@
 void setup() {
-<<<<<<< HEAD
-  // put your setup code here, to run once:
 Serial.begin(9600);
+  timerSetup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-sendTempString();
-=======
-  ldrSetup();
-  Serial.begin(9600);
+  timerLoop();
 }
 
-void loop() {
-  Serial.println(getLDRValue());
->>>>>>> ddf972e833cf203b481a5701d7825da0744a2f51
 
-Serial.println(readTempValueRAW());
-}
