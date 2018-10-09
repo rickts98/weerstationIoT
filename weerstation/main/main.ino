8ed45669e6976f656c99d040500936a22fa34bc8
Rens Aerts <rens.aerts@ziggo.nl>
2018-10-09 16:10:17 +0200

    Update weerstation/main/main.ino

--- weerstation/main/main.ino
+++ weerstation/main/main.ino
@@ -1,14 +1,35 @@
 void setup() {
-Serial.begin(9600);
+  Serial.begin(9600);
   timerSetup();
   hardwareSetup();
 }
 
 void loop() {
   timerLoop();
-  sendTempString();
+  buttonLoop();
+
+  if (buttonDown()) {
+    testComponents();
+  }
 }
 
-void hardwareSetup(){
+void hardwareSetup() {
   ldrSetup();
+  ledControlSetup();
+  buttonSetup();
+}
+
+void testComponents() {
+  for (int i = 0; i < getNrLeds(); i++) {
+    ledControlAan(i);
+    delay(500);
+    ledControlUit(i);
   }
+  Serial.print("Lichtsterkte: ");
+  Serial.println(getLDRValue());
+  Serial.print("Temperaturr: ");
+  Serial.println(readTempValue());
+
+
+}
+
