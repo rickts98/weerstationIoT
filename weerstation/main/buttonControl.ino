a034aeb27597fc5ff721c01fccdd6f6bc9fd8f70
rickts98 <ricktersteege@gmail.com>
2018-10-25 11:09:46 +0200

    Update weerstation/main/buttonControl.ino

--- weerstation/main/buttonControl.ino
+++ weerstation/main/buttonControl.ino
@@ -1,22 +1,30 @@
-const int BUTTONPIN = 8;
 
+//Variabelen
+const int BUTTONPIN = 8;
 boolean buttonPressed;
 
+//setup van knop 
 void buttonSetup(){
   pinMode(BUTTONPIN, INPUT);
+
+  //waarde van buttonPressed aanpassen
   buttonPressed = false;
 }
 
 void buttonLoop(){
+  //controleren of er op de knop gedrukt is
   if(buttonDown()){
     buttonPressed = true;
   }
 }
 
+
+//returned de waarde van buttonPressed
 boolean getButtonPressed() {
   return buttonPressed;
 }
 
+//controleerd of er op de knop gedrukt is
 boolean buttonDown(){
   return (digitalRead(BUTTONPIN) == LOW);
 }
